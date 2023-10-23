d56dca8d91c2df5211984863a08a5c9fff4f9a34
ajay-kushvah-wtt <146094360+ajay-kushvah-wtt@users.noreply.github.com>
2023-10-23 18:07:14 +0530

    station search bu location_id

--- core/gql_queries.py
+++ core/gql_queries.py
@@ -15,16 +15,16 @@ from .utils import prefix_filterset
 class StationGQLType(DjangoObjectType):
     class Meta:
         model = Station
-        interfaces = (graphene.relay.Node,)
         filter_fields = {
             "id": ["exact"],
             "name": ["exact", "icontains"],
-            "location__id": ["exact"],
+            "location": ["exact"],
             "location__uuid": ["exact"],
             "location__type": ["exact"],
             "location__code": ["exact", "icontains"],
             "location__name": ["exact", "icontains"],
         }
+        interfaces = (graphene.relay.Node,)
         connection_class = ExtendedConnection
 
 
